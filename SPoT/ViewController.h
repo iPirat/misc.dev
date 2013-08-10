269c94bc9c1b97026e1babec32f18a33579bacd4
Sergei Haller <shaller@Sergei-Hallers-Mac.local>
2013-08-10 09:13:11 +0200

    Initial Commit

---
+++ SPoT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SPoT
+//
+//  Created by Sergei Haller on 10.08.13.
+//  Copyright (c) 2013 Sergei Haller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SPoT/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SPoT
+//
+//  Created by Sergei Haller on 10.08.13.
+//  Copyright (c) 2013 Sergei Haller. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
